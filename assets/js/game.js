d818553ce9ef4cba706cbc357af0d8b3f1b30755
Ridercogswell <ridercogswell@gmail.com>
2022-01-12 00:00:46 -0800

    fix mess up

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,10 +9,20 @@ var randomNumber = function(min, max) {
   
   // fight function (now with parameter for enemy's object holding name, health, and attack values)
   var fight = function(enemy) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
+    if (Math.random() > 0.5) {
+      isPlayerTurn = false;
+    }
+
     while (playerInfo.health > 0 && enemy.health > 0) {
+      if (isPlayerTurn) {
+
+      }
       // ask player if they'd like to fight or run
       var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-  
+      
+      
       // if player picks "skip" confirm and then stop the loop
       if (promptFight === "skip" || promptFight === "SKIP") {
         // confirm player wants to skip
